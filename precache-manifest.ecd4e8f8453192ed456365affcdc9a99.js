72f727b597e4ee6a8e457fc7528c6add2e5bcd4e
insungsong <song22861@naver.com>
2020-03-23 14:55:07 +0900

    Updates

---
+++ precache-manifest.ecd4e8f8453192ed456365affcdc9a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536e8b95eda2f1786c66711e7a49623e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b4e6c63890e69a3a4321",
+    "url": "/movie_app/static/css/main.cf65d356.chunk.css"
+  },
+  {
+    "revision": "42901d470a654707de4f",
+    "url": "/movie_app/static/js/2.54999406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.54999406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e6c63890e69a3a4321",
+    "url": "/movie_app/static/js/main.1fb8fdfd.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
